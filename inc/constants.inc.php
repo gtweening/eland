da2652e3cd0ab2df2afd8f62e09e008d4de89416
gtweening <gtweening@hotmail.com>
2016-03-16 20:30:07 +0100

    Added files via upload

--- inc/constants.inc.php
+++ inc/constants.inc.php
@@ -2,13 +2,13 @@
     // Database credentials
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
-    define('DB_PASS', '');  //your password
-    define('DB_NAME', '');  //your databasename
+    define('DB_PASS', 'Nemov1975');
+    define('DB_NAME', 'ObstacleLogDB');
     
     define("CAN_REGISTER", "any");
     define("DEFAULT_ROLE", "member");
     
     define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
     //path to images
-    $imgPath = "img/Obstacles/";
+    $imgPath = "../img/Obstacles/";
 ?>
