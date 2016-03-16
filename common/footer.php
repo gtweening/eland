193f5b2d4c436c8a48e337971622a1a36b6f9598
gtweening <gtweening@hotmail.com>
2016-03-16 09:20:42 +0100

    Added files via upload

---
+++ common/footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <div id="footer">
+        footer
+    </div>
+    
+</html>
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
