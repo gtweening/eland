9a1155ae4f1f67ca4d8cae8ed9546237c2d3771b
gtweening <gtweening@hotmail.com>
2016-03-16 10:12:44 +0100

    Added files via upload

--- common/header.php
+++ common/header.php
@@ -3,14 +3,14 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link rel="stylesheet" type="text/css" href="css/StyleSheet.css">
+        <link rel="stylesheet" type="text/css" href="../css/StyleSheet.css">
         <title>The ObstacleRun Audit log</title>
     </head>
     <body>
         <div id="main">
             <div id="header">
-                <img align ="left" src="img/logo.jpg" alt="Logo" width="152" height="50">
-                <a href="inc/logout.php">uitloggen</a>
+                <img align ="left" src="../img/logo.jpg" alt="Logo" width="152" height="50">
+                <a href="../inc/logout.php">uitloggen</a>
             </div>
         </div>
     
--- common/leftColumn.php
+++ common/leftColumn.php
@@ -4,42 +4,42 @@
     <ul >       
         <li>
             <a href="checkpoints.php" id="navCheckpoints" title ="Controlepunten">
-                <img src="img/controlepunten.png" alt="Materialen" width="35" height="35" align="middle">
+                <img src="../img/controlepunten.png" alt="Materialen" width="35" height="35" align="middle">
             </a>
         </li>
         <li>
             <a href="materials.php" id="navMaterials" title ="Materialen ">
-                <img src="img/material.png" alt="Materialen" width="35" height="35" align="middle">
+                <img src="../img/material.png" alt="Materialen" width="35" height="35" align="middle">
                 
             </a>
         </li>
         <li>
             <a href="sections.php" id="navSections" title ="Hindernissen">
-                <img src="img/obstacle.png" alt="Materialen" width="35" height="35" align="middle">
+                <img src="../img/obstacle.png" alt="Materialen" width="35" height="35" align="middle">
                 
             </a>
         </li>
         <li>
             <a href="obstacleChkQ.php" id="navChkQ" title ="Controle agenda">
-                <img src="img/calendar.png" alt="Materialen" width="35" height="35" align="middle">
+                <img src="../img/calendar.png" alt="Materialen" width="35" height="35" align="middle">
                 
             </a>
         </li>
         <li>
             <a href="tocheck.php" id="navchkList" title="Overzicht te controleren">
-                <img src="img/toCheck.png" alt="Materialen" width="35" height="35" align="middle">
+                <img src="../img/toCheck.png" alt="Materialen" width="35" height="35" align="middle">
                 
             </a>
         </li>
         <li>
             <a href="Checked.php" id="navchked" title="Invoeren gecontroleerd">
-                <img src="img/checklist.png" alt="Materialen" width="35" height="35" align="middle">
+                <img src="../img/checklist.png" alt="Materialen" width="35" height="35" align="middle">
                 
             </a>
         </li>
         <li>
-            <a href="reportAll.php" id="navchked" title="Volledig overzicht t.b.v. archief">
-                <img src="img/reportAll.png" alt="Materialen" width="35" height="35" align="middle">
+            <a href="php/reportAll.php" id="navchked" title="Volledig overzicht t.b.v. archief">
+                <img src="../img/reportAll.png" alt="Materialen" width="35" height="35" align="middle">
                 
             </a>
         </li>
