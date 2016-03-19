cbc8f755d30345be4f76914721e3c138206cde4a
gtweening <gtweening@hotmail.com>
2016-03-19 17:15:47 +0100

    Added files via upload

--- common/footer.php
+++ common/footer.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html>
-    <div id="footer">
-        footer
+    <div id="footer"><br>
+      Eland - Copyright (C) 2013 - Gerko Weening <br><br>
+      This program comes with ABSOLUTELY NO WARRANTY; for details see <a href="license.txt">license</a>.<br>
+      This is free software, and you are welcome to redistribute it
+      under certain conditions; see <a href="license.txt">license</a> for details.
     </div>
     
 </html>
