d1af90bc42b506f10c17c7281708cbd1ff1e7a05
gtweening <gtweening@hotmail.com>
2016-09-09 10:26:12 +0200

    Add files via upload

---
+++ common/leftColumnBeheerder.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<div id="LeftColumn">
+    <ul >       
+        <li>
+            <a href="beheerder.php" id="navGebruikers" title ="Gebruikersbeheer">
+                <img src="../img/gebruikers.png" alt="gebruikers" width="35" height="35" align="middle">
+            </a>
+        </li>
+        <li>
+            <a href="beheerTerreinen.php" id="navTerreinen" title ="Terreinbeheer ">
+                <img src="../img/terreinen.jpg" alt="terreinen" width="35" height="35" align="middle">
+                
+            </a>
+        </li>
+			<li>
+            <a href="beheerGebruikersTerreinen.php" id="navGebruikersTerreinen" title ="GebruikersTerreinbeheer ">
+                <img src="../img/terreinen.jpg" alt="terreinen" width="35" height="35" align="middle">
+                
+            </a>
+        </li>
+    </ul>
+ </div>
+</html> 
+
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
