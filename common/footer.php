03d0c74b60de03165502370cd16dc88c38a0706f
gerko <gtweening@hotmail.com>
2019-02-22 13:27:51 +0100

    added environment for degugging

--- common/footer.php
+++ common/footer.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html>
-    <div id="footer"><br>
-      Eland - Copyright (C) 2013-2018 - Gerko Weening <br><br>
-      Dit Programma is verspreid met de hoop dat het nuttig zal zijn maar 
-		ZONDER EENDER WELKE GARANTIE; zelfs zonder de impliciete garantie van 
-		VERKOOPBAARHEID of GESCHIKTHEID VOOR EEN BEPAALD DOEL. Zie de 
-		<a href="license.txt">GNU Algemene Publieke Licentie</a> voor meer details.<br><br>
-
-      Dit programma is vrije software. Toestemming wordt verleend tot het verspreiden
-      en/of wijzigen onder zekere bepalingen; zie <a href="license.txt">licentie</a> 
-      voor details.
-    </div>
+  <div id="footer"><br>
+    <?php if (ENVIRONMENT <> 'production') { ?>
+      <div id="footer_dev">
+          <b>SESSIE:</b>  <?php echo ENVIRONMENT;  ?> <br/>
+          <b> POST: </b><br/>
+          <?php print_r($_POST); ?><br/><br/>
+          <b> VARIABELEN: </b><br/>
+          <?php echo '<pre>'.print_r(get_defined_vars(),true).'</pre>'; ?>
+      </div>
+    <?php } ?>
+   </div>
     
 </html>
--- inc/constants.inc.php
+++ inc/constants.inc.php
@@ -9,6 +9,33 @@
     define("DEFAULT_ROLE", "member");
     
     define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+
+    //Environment
+    define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+    
+    if (! defined('ENVIRONMENT') )
+    {
+    $domain = strtolower($_SERVER['HTTP_HOST']);
+
+    switch($domain) {
+
+        case 'test.uvponline.nl' :
+            define('ENVIRONMENT', 'staging');
+            error_reporting(E_ALL);
+            break;
+
+        case 'hindernislogboek.survivalbond.nl' :
+                define('ENVIRONMENT', 'production');
+                error_reporting(0);
+                break;
+
+        default :
+                define('ENVIRONMENT', 'development');
+                error_reporting(E_ALL);
+                break;
+        }
+    }
+
     //path to images
     $imgPath = "../img/Obstacles/";
 ?>
